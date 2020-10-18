4a89293749f19c7759b658df24f7c5c705016057
Vladimir Veraksa <vladimirveraksa@gmail.com>
2020-10-18 17:02:41 +0200

    ref: code refactor

--- src/app.js
+++ src/app.js
@@ -12,7 +12,6 @@ const app = express();
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
 
 app.use(express.json());
-
 app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 let mock = '';
